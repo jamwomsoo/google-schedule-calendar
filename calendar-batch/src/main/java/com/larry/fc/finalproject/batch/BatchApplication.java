e4250669bfb23316693d73a165952542eb996c9f
sungsoo <tjdtn9692@gmail.com>
2024-06-13 20:57:21 +0900

    batch job

---
+++ calendar-batch/src/main/java/com/larry/fc/finalproject/batch/BatchApplication.java
@@ -0,0 +1,2 @@
+package com.larry.fc.finalproject.batch.job;public class BatchApplication {
+}
---
+++ calendar-batch/src/main/java/com/larry/fc/finalproject/batch/job/AllReadJobConfiguration.java
@@ -0,0 +1,4 @@
+package com.larry.fc.finalproject.batch.job;
+
+public class AllReadJobConfiguration {
+}
